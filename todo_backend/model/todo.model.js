01afccde264459ef6a3f28d68a321c70e0156244
Md Jawed <mdjawedh22@gmail.com>
2023-12-11 02:15:13 +0530

    todo backend

---
+++ todo_backend/model/todo.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    status: { type: Boolean, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Todomodel = mongoose.model("tododetails", TodoSchema);
+
+module.exports = {
+  Todomodel,
+};
